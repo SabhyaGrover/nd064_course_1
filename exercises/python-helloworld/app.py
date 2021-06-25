0b7e12f2df5ccdd549196ca2a7832ff54ef94df8
SabhyaGrover <sabgrohyaver@gmail.com>
2021-06-25 11:56:23 +0530

    Complete Application Logging

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,28 @@
 from flask import Flask
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/status")
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
+
+@app.route("/metrics")
+def metrics():
+    return make_response(
+        'data: {UserCount: 140, UserCountActive: 23}',
+        200
+    )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
